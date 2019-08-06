edb3b13c188208305dd12883ca37799cafbabd41
Archika Vyas <53368347+archika20@users.noreply.github.com>
2019-08-06 12:30:27 +0530

    Create Triangle.py
    Program to check whether a triangle is possible or not using given parameters of perpendicular, base and hypotenuse.

---
+++ Triangle.py
@@ -0,0 +1,12 @@
+a = int(input("Enter Value of base "))
+b = int(input("Enter Value of perpendicular "))
+c = int(input("Enter Value of hypotenuse "))
+
+
+def check_triangle(a,b,c):
+	if(a**2+b**2==(c**2)):
+		print("Yes You can make a triangle with these sticks")
+	else:
+			print("Ever Heard about pythogoras theorem?")
+	
+check_triangle(a,b,c)
